b63f698c9b991bb7126061b76ca0fabbbe2737fa
waqar <wakku9780@gmail.com>
2024-03-07 19:12:46 +0530

    Add project files.

---
+++ InstallingBootStrap/Program.cs
@@ -0,0 +1,25 @@
+namespace InstallingBootStrap
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            var app = builder.Build();
+
+            app.UseStaticFiles();
+
+            //app.MapGet("/", () => "Hello World!");
+
+            app.MapControllerRoute(
+                name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
+            );
+
+            
+
+            app.Run();
+        }
+    }
+}
